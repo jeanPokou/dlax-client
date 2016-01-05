0845d5706474735634e6dccd48cf49f833e04ae9
jeanpokou <pokoujeanmarc@gmail.com>
2016-01-05 16:13:27 -0400

    adding dlax-card element

--- tasks/polymer-build.js
+++ tasks/polymer-build.js
@@ -56,9 +56,9 @@ gulp.task('serve',function() {
   browserSync({
     port: 5000,
     server: {
-      baseDir: 'app',
+      baseDir: './',
       routes: {
-        '/bower_components': 'bower_components'
+        'app/bower_components': 'bower_components'
       }
     }
   });
